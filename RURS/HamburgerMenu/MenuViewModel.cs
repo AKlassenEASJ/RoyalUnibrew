13448968b9ff8f72fca7ef320fcfa9da64324dc8
Line Phoebe Wienke <line644s@edu.easj.dk>
2019-04-07 15:37:33 +0200

    HamburgerMenu

---
+++ RURS/HamburgerMenu/MenuViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using RURS.Annotations;
+
+namespace RURS.HamburgerMenu
+{
+    class MenuViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<NavigationViewItemBase> NavigationItems { get; set; }
+
+        private NavigationViewItemBase _selectedItem;
+
+        public NavigationViewItemBase SelectedItem
+        {
+            get { return _selectedItem; }
+            set
+            {
+                _selectedItem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MenuViewModel()
+        {
+            NavigationItems = new ObservableCollection<NavigationViewItemBase>();
+
+            GetNagivationItems();
+
+            SelectedItem = NavigationItems.First(x => x.GetType() == typeof(NavigationViewItem));
+
+        }
+
+        private void GetNagivationItems()
+        {
+            NavigationItems.Add(new NavigationViewItem { Content = "Home", Icon = new SymbolIcon(Symbol.Home), Tag = typeof(MainPage) });
+           
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ RURS/HamburgerMenu/NavigationService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace RURS.HamburgerMenu
+{
+    class NavigationService
+    {
+        public static Frame NavigationFrame { get; set; }
+
+        public static void Navigate(Type pageType)
+        {
+            NavigationFrame.Navigate(pageType);
+            ChangeMenuSelection();
+        }
+
+        public static void GoBack()
+        {
+            NavigationFrame.GoBack();
+            ChangeMenuSelection();
+        }
+
+        private static void ChangeMenuSelection()
+        {
+            if ((Windows.UI.Xaml.Window.Current.Content as Frame).Content is MenuPage mp)
+            {
+                MenuViewModel VM = mp.DataContext as MenuViewModel;
+                Type t = NavigationFrame.Content.GetType();
+                NavigationViewItemBase tmp = (VM.NavigationItems as IEnumerable<NavigationViewItemBase>).FirstOrDefault(x => (x.Tag as Type) == t);
+                if (tmp != null)
+                    VM.SelectedItem = tmp;
+            }
+        }
+    }
+}
