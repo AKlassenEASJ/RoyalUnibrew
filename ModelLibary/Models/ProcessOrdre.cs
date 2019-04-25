a5aef3f6554149021c08af92f5cd21a8e027156d
Thomas Fogh <thom20m1@edu.easj.dk>
2019-04-26 00:37:53 +0200

    Oprettede processordre classes

---
+++ ModelLibary/Models/ProcessOrdre.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibary.Models
+{
+    class ProcessOrdre
+    {
+        private int _processOrdreNr;
+        private int _faerdigVareNr;
+        private DateTime _dato;
+        private int _kolonne;
+
+        ProcessOrdre(int processOrdre, int faerdigVareNr, DateTime dato)
+        {
+            _processOrdreNr = processOrdre;
+            _faerdigVareNr = faerdigVareNr;
+            _dato = dato;
+            _kolonne = 4;
+        }
+
+    }
+}
--- RURS/HamburgerMenu/MenuViewModel.cs
+++ RURS/HamburgerMenu/MenuViewModel.cs
@@ -42,6 +42,7 @@ namespace RURS.HamburgerMenu
         {
             NavigationItems.Add(new NavigationViewItem { Content = "Home", Icon = new SymbolIcon(Symbol.Home), Tag = typeof(MainPage) });
             //tilføj sider under her, ligesom oppeover
+            NavigationItems.Add(new NavigationViewItem { Content = "Processordre", Icon = new SymbolIcon(Symbol.Add), Tag = typeof(ProcessOrdrePage) });
             NavigationItems.Add(new NavigationViewItem {Content = "Tappe Kontrol", Icon = new SymbolIcon(Symbol.Filter), Tag = typeof(TappeKontrolPage)});
         }
 
