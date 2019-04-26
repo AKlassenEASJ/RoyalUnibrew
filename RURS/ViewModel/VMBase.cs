9dc019d13e6e59c070e4f37d24ab0a7f06499b5b
Thomas Fogh <thom20m1@edu.easj.dk>
2019-04-26 10:10:39 +0200

    Added VMBase + ModelLibary in RURS, removed phantom ModelLibrary

---
+++ RURS/ViewModel/VMBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLibary.Models;
+
+namespace RURS.ViewModel
+{
+    class VMBase
+    {
+        private ProcessOrdre _processOrdre;
+    }
+}
