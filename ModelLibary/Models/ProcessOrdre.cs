cfd48ae9eb0eb3d6d0f4465240cc64dabcffd98f
Thomas Fogh <thom20m1@edu.easj.dk>
2019-04-26 10:34:46 +0200

    Oprettede Rest relaterede classes for ProcessOrdre

--- ModelLibary/Models/ProcessOrdre.cs
+++ ModelLibary/Models/ProcessOrdre.cs
@@ -8,18 +8,22 @@ namespace ModelLibary.Models
 {
     public class ProcessOrdre
     {
+        #region InstanceFields
         private int _processOrdreNr;
         private int _faerdigVareNr;
         private DateTime _dato;
         private int _kolonne;
+        #endregion
 
-        ProcessOrdre(int processOrdre, int faerdigVareNr, DateTime dato)
+        #region constructors
+        public ProcessOrdre(int processOrdre, int faerdigVareNr, DateTime dato)
         {
             _processOrdreNr = processOrdre;
             _faerdigVareNr = faerdigVareNr;
             _dato = dato;
             _kolonne = 4;
         }
+        #endregion
 
     }
 }
---
+++ REST Service/DBUtil/ProcessOrdreManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace REST_Service.DBUtil
+{
+    public class ProcessOrdreManager
+    {
+    }
+}
