9c7a9a6f4b3cb9efe897870225d5f69d89785081
Anders Genderskov Binder <ande972k@edu.easj.dk>
2019-04-29 10:42:14 +0200

    Har tilføjet RelayCommand til begge applikations projekter

---
+++ AdminRURS/ViewModel/AnsatViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminRURS.ViewModel
+{
+    class AnsatViewModel
+    {
+
+        #region InstanceFields
+
+
+
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+
+        #region Constructor
+
+        public AnsatViewModel()
+        {
+            
+        }
+
+
+        #endregion
+
+        #region Methods
+
+        
+
+        #endregion
+
+
+
+
+
+
+    }
+}
