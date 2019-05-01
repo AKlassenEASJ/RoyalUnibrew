0944395176da58ab147aa9824e99c871d6a4fb8d
Line Phoebe Wienke <line644s@edu.easj.dk>
2019-05-01 11:50:35 +0200

    Rettede radiobutton til checkbox

---
+++ RURS/Handler/TappeKontrolHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using RURS.ViewModel;
+
+namespace RURS.Handler
+{
+    class TappeKontrolHandler
+    {
+        private TappeKontrolViewModel _viewModel;
+
+        public TappeKontrolHandler(TappeKontrolViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public void Clear()
+        {
+            //foreach (var VARIABLE in radio)
+            //{
+                
+
+            //}
+        }
+
+        private void findRadioButton(List<RadioButton> list)
+        {
+            
+        }
+    }
+}
--- RURS/ViewModel/TappeKontrolViewModel.cs
+++ RURS/ViewModel/TappeKontrolViewModel.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using ModelLibrary.Models;
+using RURS.Common;
+using RURS.Handler;
 
 namespace RURS.ViewModel
 {
     class TappeKontrolViewModel : VMBase
     {
+        public TappeKontrolHandler Handler { get; set; }
+
         private TappeKontrol _selectedTappeKontrol;
 
         public TappeKontrol SelectedTappeKontrol
@@ -21,9 +26,14 @@ namespace RURS.ViewModel
             }
         }
 
+        public ICommand ClearCommand { get; set; }
+
         public TappeKontrolViewModel()
         {
+            Handler = new TappeKontrolHandler(this);
             SelectedTappeKontrol = new TappeKontrol() {Tidspunkt = DateTime.Now};
+            //ClearCommand = new RelayCommand();
+
         }
     }
 }
