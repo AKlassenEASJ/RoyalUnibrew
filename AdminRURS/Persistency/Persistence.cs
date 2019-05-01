062bdedb155dc795c2f9cdf33734cc940a1709a0
Anders Genderskov Binder <ande972k@edu.easj.dk>
2019-05-01 10:11:59 +0200

    Omdøbt Persistence

---
+++ AdminRURS/Persistency/Persistence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminRURS.Persistency
+{
+    class Persistence<T>
+    {
+
+        #region InstanceFields
+
+        private const string URI = "";
+
+
+        #endregion
+
+
+        #region Methods
+
+        public 
+
+
+        #endregion
+
+
+    }
+}
