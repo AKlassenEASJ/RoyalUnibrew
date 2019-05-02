d431e215f97d6ba8e632907380c0031693383238
Thomas Fogh <thom20m1@edu.easj.dk>
2019-05-02 13:40:27 +0200

    Added Tests

---
+++ REST Service.Tests/DBUtil/ProcessOrdreManagerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelLibrary.Models;
+using REST_Service.DBUtil;
+
+namespace REST_Service.Tests.DBUtil
+{
+    [TestClass]
+    public class ProcessOrdreManagerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
