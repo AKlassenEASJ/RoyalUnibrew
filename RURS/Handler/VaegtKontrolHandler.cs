51ce6bb93556e4b8caeb86a6d7a49238dc95dbb1
CBoens <43374935+CBoens@users.noreply.github.com>
2019-05-03 13:14:42 +0200

    vaegtkontrol handler oprettet og påbegyndt

--- RURS/Handler/VaegtKontrolHandler.cs
+++ RURS/Handler/VaegtKontrolHandler.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using ModelLibary.Models;
+using RURS.Persistency;
+using RURS.ViewModel;
 
 namespace RURS.Handler
 {
     class VaegtKontrolHandler
     {
+        public VaegtKontrolViewModel VaegtKontrolViewModel { get; set; }
+
+        public VaegtKontrolHandler(VaegtKontrolViewModel vaegtKontrolViewModel)
+        {
+            VaegtKontrolViewModel = vaegtKontrolViewModel;
+        }
+
+        public void CreateVaegtKontrol()
+        {
+            //skal oprettes async
+            int processOrdreNr = VaegtKontrolViewModel.ProcessOrdre.ProcessOrdreNr;
+            int kontrolNr = VaegtKontrolViewModel.NyVaegtKontrol.KontrolNr;
+            DateTime datoTid = DateTime.Now;
+            VaegtKontrol aVaegtKontrol = new VaegtKontrol(processOrdreNr,kontrolNr,datoTid);
+
+
+
+        }
+
+
+
     }
 }
