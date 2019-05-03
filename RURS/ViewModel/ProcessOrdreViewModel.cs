225fa5f992784ce4286024a36ac7edeb260a6f03
Thomas Fogh <thom20m1@edu.easj.dk>
2019-05-03 10:16:35 +0200

    Set up POViewModel

--- RURS/ViewModel/ProcessOrdreViewModel.cs
+++ RURS/ViewModel/ProcessOrdreViewModel.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ModelLibary.Models;
 
 namespace RURS.ViewModel
 {
     class ProcessOrdreViewModel:VMBase
-
     {
+        private ProcessOrdre _selectedProcessOrdre;
+
     }
 }
