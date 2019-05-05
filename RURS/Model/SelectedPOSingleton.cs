a660bf7b891aee94680a2d8f40ff2f1e19489e4d
Thomas Fogh <thom20m1@edu.easj.dk>
2019-05-05 21:10:41 +0200

    PO: Delvis opsætning af Persistency

---
+++ RURS/Model/SelectedPOSingleton.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLibary.Models;
+using RURS.Annotations;
+
+namespace RURS.Model
+{
+    class SelectedPOSingleton
+    {
+
+        private static ProcessOrdre _processOrdreSingleTonInstans = null;
+
+        private SelectedPOSingleton(ProcessOrdre processOrdre)
+        {
+            _processOrdreSingleTonInstans.Dato = processOrdre.Dato;
+            _processOrdreSingleTonInstans.FaerdigVareNr = processOrdre.FaerdigVareNr;
+            _processOrdreSingleTonInstans.ProcessOrdreNr = processOrdre.ProcessOrdreNr;
+        }
+        
+
+        public static ProcessOrdre POSingletonInstans
+        {
+            get
+            {
+                return _processOrdreSingleTonInstans;
+            }
+            set
+            {
+                if (_processOrdreSingleTonInstans == null)
+                {
+                    new SelectedPOSingleton(value);
+                }
+                else
+                {
+                    _processOrdreSingleTonInstans = value;
+                }
+                
+            }
+        }
+
+        public void setDato(DateTime dato)
+        {
+            _processOrdreSingleTonInstans.Dato = dato;
+        }
+        public void setPONr(int processOrdreNummer)
+        {
+            _processOrdreSingleTonInstans.ProcessOrdreNr = processOrdreNummer;
+        }
+        public void setFVNr(int faerdigVareNummer)
+        {
+            _processOrdreSingleTonInstans.FaerdigVareNr = faerdigVareNummer;
+        }
+        
+    }
+}
---
+++ RURS/Persistency/PersistencyProcessOrdre.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Chat;
+using Windows.System;
+using Windows.UI.Xaml;
+using ModelLibary.Models;
+using Newtonsoft.Json;
+
+namespace RURS.Persistency
+{
+    class PersistencyProcessOrdre
+    {
+        private const string URI = "http://localhost:60096/api/ProcessOrdre";
+
+        public bool Post(ProcessOrdre processOrdre)
+        {
+            bool sucess=true;
+
+            using (HttpClient client = new HttpClient())
+            {
+                string SerializedProcessOrdre = JsonConvert.SerializeObject(processOrdre);
+                StringContent content = new StringContent(SerializedProcessOrdre, Encoding.UTF8, "application/json");
+
+                Task<HttpResponseMessage> postAsync = client.PostAsync(URI, content);
+
+
+                HttpResponseMessage resp = postAsync.Result;
+                if (resp.IsSuccessStatusCode)
+                {
+                    string jsonResString = resp.Content.ReadAsStringAsync().Result;
+                    sucess = JsonConvert.DeserializeObject<bool>(jsonResString);
+                }
+                else
+                {
+                    sucess = false;
+                }
+                
+                return sucess;
+            }
+
+
+
+
+
+            return sucess;
+        }
+
+        public ProcessOrdre GetOne(int id)
+        {
+            ProcessOrdre processOrdre = new ProcessOrdre();
+
+
+            using (HttpClient client = new HttpClient())
+            {
+                Task<string> resTask = client.GetStringAsync(URI + $"/{id}");
+                string jsonStr = resTask.Result;
+
+                processOrdre = JsonConvert.DeserializeObject<ProcessOrdre>(jsonStr);
+            }
+
+            return processOrdre;
+        }
+
+        public List<ProcessOrdre> GetAll()
+        {
+            List<ProcessOrdre> processOrdrer = new List<ProcessOrdre>();
+            
+
+            using (HttpClient client = new HttpClient())
+            {
+                Task<string> resTask = client.GetStringAsync(URI);
+                string jsonStr = resTask.Result;
+
+                processOrdrer = JsonConvert.DeserializeObject<List<ProcessOrdre>>(jsonStr);
+            }
+
+            return processOrdrer;
+        }
+    }
+}
