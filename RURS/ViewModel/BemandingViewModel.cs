c2b0fe2ade2cf7e9a96659649d160e001219581b
Anders Genderskov Binder <ande972k@edu.easj.dk>
2019-05-14 15:22:22 +0200

    Oprettet klasse BemandingViewModel

---
+++ RURS/ViewModel/BemandingViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RURS.ViewModel
+{
+    class BemandingViewModel : VMBase
+    {
+        #region InstanceFields
+        
+
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Constructor
+
+        public BemandingViewModel()
+        {
+            
+        }
+
+
+        #endregion
+
+        #region Methods
+
+        
+
+        #endregion
+
+    }
+}
