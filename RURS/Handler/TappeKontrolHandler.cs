3d796fd3185375550ced3846b8862e435d8e9821
Line Phoebe Wienke <line644s@edu.easj.dk>
2019-05-15 23:23:18 +0200

    TappeKontrol Validation + BackButton Enabled

--- RURS/Handler/TappeKontrolHandler.cs
+++ RURS/Handler/TappeKontrolHandler.cs
@@ -5,7 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
+using RURS.Common;
+using RURS.Model;
 using RURS.Persistency;
+using RURS.Validation;
 using RURS.ViewModel;
 
 namespace RURS.Handler
@@ -16,52 +19,82 @@ namespace RURS.Handler
     class TappeKontrolHandler
     {
         private TappeKontrolViewModel _viewModel;
-        
+        private ValidationTappeKontrol _validation;
+
+        public string ErrorMessage { get; set; }
 
         public TappeKontrolHandler(TappeKontrolViewModel viewModel)
         {
             _viewModel = viewModel;
-            
+            _validation = new ValidationTappeKontrol();
         }
 
         //private Stopwatch _stopwatch = new Stopwatch();
 
         public void Add()
         {
+            ErrorMessage = null;
             //Indsætter ProcessOrderNr
             _viewModel.SelectedTappeKontrol.ProcessOrderNr = 1;
             //Indsætter tid
             _viewModel.SelectedTappeKontrol.Tidspunkt = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
             _viewModel.SelectedTappeKontrol.Tidspunkt = _viewModel.SelectedTappeKontrol.Tidspunkt + _viewModel.TimeSpan;
 
-            //Sætter Vægtkontrol til null
-            if (_viewModel.SelectedTappeKontrol.VaegtKontrol == 0)
+            foreach (FejlTjek f in _viewModel.Validatons.Values)
             {
-                _viewModel.SelectedTappeKontrol.VaegtKontrol = -1;
+                if (f.Bedsked != null)
+                {
+                    addToMessage(f.Bedsked);
+                }
             }
 
-            //Sætter SmagsTestNr til Null
-            if (_viewModel.SelectedTappeKontrol.SmagsTestNr == 0)
+            if (ErrorMessage != null)
             {
-                _viewModel.SelectedTappeKontrol.SmagsTestNr = -1;
+                MessageDialogHelper.Show(ErrorMessage, "Der mangler oplysninger");
             }
-            //Sætter C02 til Null
-            if (_viewModel.SelectedTappeKontrol.Co2Kontrol == 0)
+            else
             {
-                _viewModel.SelectedTappeKontrol.Co2Kontrol = -1;
-            }
+                if (_validation.TjekPrimærNøgle(_viewModel.SelectedTappeKontrol.ProcessOrderNr, _viewModel.SelectedTappeKontrol.Tidspunkt))
+                {
+                    MessageDialogHelper.Show("Du har allerede lavet en på dette tidspunkt", "Tappe Kontrollen findes allerede");
+                }
+                else
+                {
+                    //Sætter Vægtkontrol til null
+                    if (_viewModel.SelectedTappeKontrol.VaegtKontrol == 0)
+                    {
+                        _viewModel.SelectedTappeKontrol.VaegtKontrol = -1;
+                    }
 
-            if (PersistenceTappeKontrol.Post(_viewModel.SelectedTappeKontrol))
-            {
-                Clear();
-                //_viewModel.MiniutesLeft = 15;
+                    //Sætter SmagsTestNr til Null
+                    if (_viewModel.SelectedTappeKontrol.SmagsTestNr == 0)
+                    {
+                        _viewModel.SelectedTappeKontrol.SmagsTestNr = -1;
+                    }
+                    //Sætter C02 til Null
+                    if (_viewModel.SelectedTappeKontrol.Co2Kontrol == 0)
+                    {
+                        _viewModel.SelectedTappeKontrol.Co2Kontrol = -1;
+                    }
+                    if (PersistenceTappeKontrol.Post(_viewModel.SelectedTappeKontrol))
+                    {
+
+                        Clear();
+                        //_viewModel.MiniutesLeft = 15;
+                    }
+                }
+
+              
             }
+            
+            
         }
         /// <summary>
         /// Metode til at clear de fleste i tappekontrollen
         /// </summary>
         public void Clear()
         {
+            ErrorMessage = null;
             _viewModel.SelectedTappeKontrol.KontrolTemp = 0;
             _viewModel.SelectedTappeKontrol.VaeskeTemp = 0;
             _viewModel.SelectedTappeKontrol.VaegtKontrol = 0;
@@ -74,23 +107,58 @@ namespace RURS.Handler
             }
         }
 
-        //private void TimeLeft()
-        //{
-        //    _viewModel.MiniutesLeft = 0;
+        #region Validations
 
-        //    _stopwatch.Start();
-        //    while (_stopwatch.IsRunning)
-        //    {
-        //        //_stopwatch.
-        //        //if ()
-        //        //{
-                    
-        //        //}
-        //    }
+        public void TjekDasseNr()
+        {
+            _viewModel.Validatons["Daasenr"].Bedsked = _validation.TjekNr(_viewModel.SelectedTappeKontrol.DaaseNr);
+        }
+
+        public void TjekLaagNr()
+        {
+            _viewModel.Validatons["laagNr"].Bedsked = _validation.TjekNr(_viewModel.SelectedTappeKontrol.LaagNr);
+        }
+
+        public void TjekSignatur()
+        {
+            _viewModel.Validatons["Signatur"].Bedsked = _validation.Empty(_viewModel.SelectedTappeKontrol.Signatur);
+        }
+
+        public void TjekKontrolTemp()
+        {
+            _viewModel.Validatons["KontrolTemp"].Bedsked =
+                _validation.TjekTemperatur(_viewModel.SelectedTappeKontrol.KontrolTemp);
+        }
+
+        public void TjekVæskeTemp()
+        {
+            _viewModel.Validatons["VæskeTemp"].Bedsked =
+                _validation.TjekTemperatur(_viewModel.SelectedTappeKontrol.VaeskeTemp);
+        }
+
+        #endregion
+
+            //private void TimeLeft()
+            //{
+            //    _viewModel.MiniutesLeft = 0;
+
+            //    _stopwatch.Start();
+            //    while (_stopwatch.IsRunning)
+            //    {
+            //        //_stopwatch.
+            //        //if ()
+            //        //{
+
+            //        //}
+            //    }
+
+
+        private void addToMessage(string bedsked)
+        {
+            ErrorMessage = ErrorMessage + $"\n{bedsked}";
+        }
 
 
 
-
-        //}
     }
 }
---
+++ RURS/Validation/ValidationBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RURS.Validation
+{
+    class ValidationBase
+    {
+        public string IntToSmall(int tjek)
+        {
+            if (tjek < 0)
+            {
+                return "Værdien skal være større";
+            }
+
+            return null;
+        }
+
+        public string DoubleToSmall(double tjek)
+        {
+            if (tjek < 0)
+            {
+                return "Værdien skal være større";
+            }
+
+            return null;
+        }
+
+        public string Empty(string tjek)
+        {
+            if (tjek.Length < 1 || tjek == "")
+            {
+                return "Denne må ikke være tom";
+            }
+
+            return null;
+        }
+    }
+}
