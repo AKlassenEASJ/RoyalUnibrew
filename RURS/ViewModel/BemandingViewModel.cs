20c1c7c58c0fdba7ed4edb456f7c988cffa6e23e
Anders Genderskov Binder <ande972k@edu.easj.dk>
2019-05-19 23:27:36 +0200

    BemandingViewModel: Har tilføjet InstanceFields og Properties

--- RURS/ViewModel/BemandingViewModel.cs
+++ RURS/ViewModel/BemandingViewModel.cs
@@ -3,18 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using ModelLibary.Models;
 
 namespace RURS.ViewModel
 {
     class BemandingViewModel : VMBase
     {
         #region InstanceFields
-        
+
+        private DateTimeOffset _date;
+        private Bemanding _bemanding;
+        private ICommand _addCommand;
 
 
         #endregion
 
         #region Properties
+
+        public DateTimeOffset Date
+        {
+            get { return _date; }
+            set
+            {
+                _date = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public Bemanding Bemanding
+        {
+            get { return _bemanding; }
+            set { _bemanding = value; }
+        }
+
 
 
 
