2e9cad220ffd06ec9613fdede128c7100feb8d3a
Anders Genderskov Binder <ande972k@edu.easj.dk>
2019-05-20 10:42:23 +0200

    Oprettet generisk klasse PersistenceBemanding og lavet metoden PostAsync i denne.

--- RURS/Handler/BemandingHandler.cs
+++ RURS/Handler/BemandingHandler.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ModelLibary.Models;
+using RURS.Persistency;
+using RURS.ViewModel;
 
 namespace RURS.Handler
 {
     class BemandingHandler
     {
+        #region InstanceFields
+
+        private PersistenceBemanding<Bemanding> _persistence = new PersistenceBemanding<Bemanding>();
+
+        #endregion
+
+        #region Properties
+
+        public BemandingViewModel BemandingViewModel { get; set; }
+
+        #endregion
+
+        #region Contructor
+
+        public BemandingHandler(BemandingViewModel viewModel)
+        {
+            BemandingViewModel = viewModel;
+        }
+
+
+        #endregion
+
+        #region Methods
+
+        public async void AddAsync()
+        {
+            //_persistence.PostAsync()
+        }
+        
+
+        #endregion
     }
 }
