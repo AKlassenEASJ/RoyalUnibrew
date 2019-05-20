90740763222772d862daac64937358a5451bac62
Line Phoebe Wienke <line644s@edu.easj.dk>
2019-05-20 10:51:44 +0200

    DaaseVaegt Get min, snit, max i diagram metode

--- RURS/Handler/DaaseVaegtHandler.cs
+++ RURS/Handler/DaaseVaegtHandler.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RURS.Model;
 using RURS.ViewModel;
 
 namespace RURS.Handler
@@ -14,6 +16,39 @@ namespace RURS.Handler
         public DaaseVaegtHandler(DaaseVaegtViewModel viewModel)
         {
             _viewModel = viewModel;
+        }
+
+        public void add()
+        {
+
+        }
+
+        public void GetValues()
+        {
+            GetMin();
+            GetSnit();
+            GetMax();
+        }
+
+        private void GetMax()
+        {
+            _viewModel.Maximum = new ObservableCollection<Record>();
+            _viewModel.Maximum.Add(new Record(1, 352.1));
+            _viewModel.Maximum.Add(new Record(24, 352.1));
+        }
+
+        private void GetMin()
+        {
+            _viewModel.Minimum = new ObservableCollection<Record>();
+            _viewModel.Minimum.Add(new Record(1, 336.9));
+            _viewModel.Minimum.Add(new Record(24, 336.9));
+        }
+
+        private void GetSnit()
+        {
+            _viewModel.Snit = new ObservableCollection<Record>();
+            _viewModel.Snit.Add(new Record(1, 347));
+            _viewModel.Snit.Add(new Record(24, 347));
         }
     }
 }
--- RURS/ViewModel/DaaseVaegtViewModel.cs
+++ RURS/ViewModel/DaaseVaegtViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RURS.Handler;
+using RURS.Model;
 
 namespace RURS.ViewModel
 {
@@ -21,10 +23,20 @@ namespace RURS.ViewModel
             }
         }
 
-        public DaaseVaegtHandler Handler { get; set; }      
+        public DaaseVaegtHandler Handler { get; set; }
+        public ObservableCollection<Record> Maximum { get; set; }
+        public ObservableCollection<Record> Snit { get; set; }
+        public ObservableCollection<Record> Minimum { get; set; }
+        public ObservableCollection<Record> Expted { get; set; }
+        public ObservableCollection<Record> Vaegts { get; set; }
+        
+
         public DaaseVaegtViewModel()
         {
             Handler = new DaaseVaegtHandler(this);
+            Handler.GetValues();
+            Expted = new ObservableCollection<Record>();
+            Vaegts = new ObservableCollection<Record>();
         }
     }
 }
